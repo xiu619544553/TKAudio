73dc80ed34180b829a64bd754e4b3a3bb4b9c2c4
韩秀辉 <hanxiuhui@pxjy.com>
2019-08-03 15:17:35 +0800

    配置

--- TKAudio/AppDelegate.m
+++ TKAudio/AppDelegate.m
@@ -17,10 +17,13 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
     self.window.backgroundColor = UIColor.whiteColor;
     self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[ViewController new]];
     [self.window makeKeyAndVisible];
+    
     return YES;
 }
 
